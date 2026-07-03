9fc50196694947f019405dac2a5bab33038a56a7
PolandLet'sPlay <claudiagranielive77@gmail.com>
2026-07-03 19:34:45 +0200

    Add Google Tag Manager script to gtag.js

---
+++ gtag.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HZT5F74S7F"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HZT5F74S7F');
+</script>
